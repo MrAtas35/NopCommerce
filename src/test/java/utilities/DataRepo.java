85c66dae505ecbe753c92f93ef4887c25d98c602
1bo999 <ihuseynli1604@gmail.com>
2025-11-01 02:52:30 +0400

    US_506 created

--- src/test/java/utilities/DataRepo.java
+++ src/test/java/utilities/DataRepo.java
@@ -3,7 +3,7 @@ package utilities;
 import org.testng.annotations.DataProvider;
 
 public class DataRepo {
-    @DataProvider(name = "userData", parallel = true)
+    @DataProvider(name = "userData")
     public Object[][] getUserData() {
         return new Object[][]{
                 {"",},
